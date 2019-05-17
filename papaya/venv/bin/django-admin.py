8bfdcab46a7b3aba64587a21b2733d6e3ea4383e
Nicci Lee Abueg <nicci.abueg@gmail.com>
2019-05-17 13:14:07 -0600

    updating DAng for Shannon help

---
+++ papaya/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/niccithenomad/sources/DojoAssignments/Angular-Django/papaya/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
